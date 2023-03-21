3d3c9bfd7370ac638aafb17d1f235dd730256885
Suhail Alkaabi <suhail.alkaabi@gmail.com>
2023-03-20 21:06:17 -0600

    updated about and login

--- BeyondPrice-app/src/app/about/about.component.ts
+++ BeyondPrice-app/src/app/about/about.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-about',
+  selector: 'app-root',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.css']
 })
-export class AboutComponent {
+export class AppComponent {
+  aboutPopupVisible = false;
 
+  togglePopup(popupId: string): void {
+    if (popupId === 'about-popup') {
+      this.aboutPopupVisible = !this.aboutPopupVisible;
+    }
+  }
+
+  togglePopup1(popupId: string): void {
+    if (popupId === 'about-popup') {
+      this.aboutPopupVisible = !this.aboutPopupVisible;
+    }
+  }
+
+  togglePopup2(popupId: string): void {
+    if (popupId === 'about-popup') {
+      this.aboutPopupVisible = !this.aboutPopupVisible;
+    }
+  }
+
+  togglePopup3(popupId: string): void {
+    if (popupId === 'about-popup') {
+      this.aboutPopupVisible = !this.aboutPopupVisible;
+    }
+  }
+
+  togglePopup4(popupId: string): void {
+    if (popupId === 'about-popup') {
+      this.aboutPopupVisible = !this.aboutPopupVisible;
+    }
+  }
 }
