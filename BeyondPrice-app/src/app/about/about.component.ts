3be03a97c2cdce4b6bfec310d366f020d980fef1
Suhail Alkaabi <suhail.alkaabi@gmail.com>
2023-03-22 20:16:18 -0600

    updated about

--- BeyondPrice-app/src/app/about/about.component.ts
+++ BeyondPrice-app/src/app/about/about.component.ts
@@ -6,35 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
-  aboutPopupVisible = false;
+  show = {
+    about: false,
+    howItWorks: false,
+    why: false,
+    faqs: false,
+    privacyPolicy: false
+  };
 
-  togglePopup(popupId: string): void {
-    if (popupId === 'about-popup') {
-      this.aboutPopupVisible = !this.aboutPopupVisible;
-    }
+  togglePopup(popup: 'about'): void {
+    this.show[popup] = !this.show[popup];
   }
 
-  togglePopup1(popupId: string): void {
-    if (popupId === 'about-popup') {
-      this.aboutPopupVisible = !this.aboutPopupVisible;
-    }
+  togglePopup1(popup1: 'howItWorks'): void {
+    this.show[popup1] = !this.show[popup1];
   }
 
-  togglePopup2(popupId: string): void {
-    if (popupId === 'about-popup') {
-      this.aboutPopupVisible = !this.aboutPopupVisible;
-    }
+  togglePopup2(popup2: 'why'): void {
+    this.show[popup2] = !this.show[popup2];
   }
 
-  togglePopup3(popupId: string): void {
-    if (popupId === 'about-popup') {
-      this.aboutPopupVisible = !this.aboutPopupVisible;
-    }
+  togglePopup3(popup3: 'faqs'): void {
+    this.show[popup3] = !this.show[popup3];
   }
 
-  togglePopup4(popupId: string): void {
-    if (popupId === 'about-popup') {
-      this.aboutPopupVisible = !this.aboutPopupVisible;
-    }
+  togglePopup4(popup4: 'privacyPolicy'): void {
+    this.show[popup4] = !this.show[popup4];
   }
 }
