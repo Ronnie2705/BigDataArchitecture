db26f7c212f48ef1f086fb0a455e8dc8b3ce5a6c
bvjhansisaketa <bvjhansisaketa@gmail.com>
2023-03-27 15:55:21 -0600

    fixing conflicts

--- BeyondPrice-app/src/app/home/home.component.ts
+++ BeyondPrice-app/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  constructor(private router: Router) { }
   bigDealURL:string = "../assets/images/bigdeal.png"
+
+  gotoLogin() {
+    this.router.navigate(['/login']);
+  }
+  gotoSignup() {
+    this.router.navigate(['/signup']);
+  }
+  
 
 }
