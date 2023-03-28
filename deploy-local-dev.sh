b423de8e7c080c2db85f088de3a679e07aab2fb3
“swapnilsethi” <swapnilsth6@gmail.com>
2023-03-27 23:56:38 -0600

    Working code

--- deploy-local-dev.sh
+++ deploy-local-dev.sh
@@ -9,35 +9,35 @@
 # to identify the processes ids
 
 # Creating a namespace to upload all pods
-kubectl create namespace bigdata
-kubectl config set-context --current --namespace=bigdata
+kubectl create namespace bigdata123
+kubectl config set-context --current --namespace=bigdata123
 
 # redis
-kubectl -n bigdata apply -f redis/redis-deployment.yaml
-kubectl -n bigdata apply -f redis/redis-service.yaml
+kubectl -n bigdata123 apply -f redis/redis-deployment.yaml
+kubectl -n bigdata123 apply -f redis/redis-service.yaml
 
 # rabbitMq
-kubectl -n bigdata apply -f rabbitmq/rabbitmq-deployment.yaml
-kubectl -n bigdata apply -f rabbitmq/rabbitmq-service.yaml
+kubectl -n bigdata123 apply -f rabbitmq/rabbitmq-deployment.yaml
+kubectl -n bigdata123 apply -f rabbitmq/rabbitmq-service.yaml
 
 # rest
-kubectl -n bigdata apply -f rest/rest-deployment.yaml
-kubectl -n bigdata apply -f rest/rest-service.yaml
-kubectl -n bigdata apply -f rest/rest-ingress.yaml
+kubectl -n bigdata123 apply -f rest/rest-deployment.yaml
+kubectl -n bigdata123 apply -f rest/rest-service.yaml
+kubectl -n bigdata123 apply -f rest/rest-ingress.yaml
 
 # logs
-kubectl -n bigdata apply -f logs/logs-deployment.yaml
+kubectl -n bigdata123 apply -f logs/logs-deployment.yaml
 
 # worker
-kubectl -n bigdata apply -f worker/worker-deployment.yaml
+kubectl -n bigdata123 apply -f worker/worker-deployment.yaml
 
 # mongodb
-kubectl -n bigdata apply -f mongodb/mongodb-secrets.yaml
-kubectl -n bigdata create configmap mongodb-config-file --from-file=conf=mongodb/mongodb.conf
-kubectl -n bigdata apply -f mongodb/mongodb-pvc.yaml
-kubectl -n bigdata apply -f mongodb/mongodb-service.yaml
-kubectl -n bigdata apply -f mongodb/mongodb-deployment.yaml
+#kubectl -n bigdata apply -f mongodb/mongodb-secrets.yaml
+#kubectl -n bigdata create configmap mongodb-config-file --from-file=conf=mongodb/mongodb.conf
+#kubectl -n bigdata apply -f mongodb/mongodb-pvc.yaml
+#kubectl -n bigdata apply -f mongodb/mongodb-service.yaml
+#kubectl -n bigdata apply -f mongodb/mongodb-deployment.yaml
 
 # portforwarding 
 kubectl port-forward --address 0.0.0.0 service/redis 6379:6379 &
-kubectl port-forward --address 0.0.0.0 service/mongodb 27017:27017 &
+#kubectl port-forward --address 0.0.0.0 service/mongodb 27017:27017 &
