a6395ff47cf8510e25588664afab4b910a7607cc
bvjhansisaketa <bvjhansisaketa@gmail.com>
2023-03-29 15:23:47 -0600

    login connecting backend

--- BeyondPrice-app/src/app/login/login.component.ts
+++ BeyondPrice-app/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { RestService } from '../rest.service';
 
 @Component({
   selector: 'app-login',
@@ -8,28 +9,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent {
   loginForm: FormGroup;
-  submitted = false;
 
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private api:RestService) { this.loginForm = new FormGroup({
+    email: new FormControl(null, {  updateOn: 'submit' }),
+    password: new FormControl(null, {  updateOn: 'submit' }),
 
-  ngOnInit() {
-    this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
-    });
-  }
+  });}
 
-  get f() { return this.loginForm.controls; }
+  
+
 
   onSubmit() {
-    this.submitted = true;
-
-    if (this.loginForm.invalid) {
-      return;
-    }
-
-    // Call login service to authenticate user
-    // If successful, redirect to main page
-    // Otherwise, display error message
-  }
+  
+}
 }
